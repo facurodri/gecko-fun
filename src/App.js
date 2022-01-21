aab49e1674a16e9424f4da1a569a36fde3718b21
Facundo Rodriguez <facurodri91@gmail.com>
2022-01-20 21:47:09 -0500

    found a diamond in the rough

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import { BrowserRouter as Router, Route } from "react-router-dom";
+//import NavTabs from "./components/NavTabs";
+//import Home from "./components/pages/Home";
+//import Search from "./components/pages/Search";
+//import Blog from "./components/pages/Blog";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello World</h1>
     </div>
   );
 }
